79fb14b7516ab5061bf705e8f05bc5c5b38019d6
MateusOli21 <mateus@hotmail.com>
2021-01-13 16:20:31 -0300

    Created Navbar component.

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { Nav, NavDropdown } from "react-bootstrap";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+import "./styles.scss";
+
+const NavbarComponent = () => {
+  const [mobileMenu, setMobileMenu] = useState(false);
+
+  const handleClickMobileMenu = () => setMobileMenu(!mobileMenu);
+
+  return (
+    <header className="header-container">
+      <span className="logo">HUB EWZ</span>
+
+      <div className="mobile-menu" onClick={handleClickMobileMenu}>
+        {mobileMenu ? <FaTimes /> : <FaBars />}
+      </div>
+
+      <Nav
+        className="nav-container"
+        style={{ right: mobileMenu ? "0" : "-999px" }}
+      >
+        <ul>
+          <li>Home</li>
+          <li>Coldcall</li>
+          <li>Produtos</li>
+          <li>Renda Variável</li>
+          <NavDropdown title="IP" id="collasible-nav-dropdown">
+            <NavDropdown.Item href="#">Imobiliário</NavDropdown.Item>
+            <NavDropdown.Item href="#">Saúde</NavDropdown.Item>
+            <NavDropdown.Item href="#">Vida</NavDropdown.Item>
+          </NavDropdown>
+          <li>Câmbio</li>
+          <NavDropdown title="Operacional" id="collasible-nav-dropdown">
+            <NavDropdown.Item href="#">Marketing</NavDropdown.Item>
+            <NavDropdown.Item href="#">Recursos Humanos</NavDropdown.Item>
+            <NavDropdown.Item href="#">Tecnologia</NavDropdown.Item>
+          </NavDropdown>
+        </ul>
+      </Nav>
+    </header>
+  );
+};
+
+export default NavbarComponent;
