2ca28d3236c16765ac8f606f2eef0c893efcd971
unknown <lucasvini193@hotmail.com>
2021-01-14 13:38:07 -0300

    Atualização de component de area

---
+++ src/components/CustomTextArea/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './styles.scss';
+
+const CustomTextArea = ({name}) => {
+    return(
+
+
+        <div className="responsive-container custom-area">
+            <label>Informações</label>
+           <textarea>
+        
+            </textarea>
+        </div>   
+    )
+};
+
+export default CustomTextArea;
