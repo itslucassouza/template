1046662b02174774181864797a27ef8858782245
MateusOli21 <mateus@hotmail.com>
2021-01-14 14:25:36 -0300

    Adjusted some errors on App and technology page.

--- src/pages/Technology/index.js
+++ src/pages/Technology/index.js
@@ -1,34 +1,31 @@
 import React from 'react';
 
 import Column from '../../components/Column';
-// import ContentWrapper from '../../components/ContentWrapper';
 import CustomInput from '../../components/CustomInput';
-
 import CustomTextArea from '../../components/CustomTextArea';
-
 
 import './styles.scss';
 
 const Technology = () => {
-    const teste = 'algumacoisa'
-    return(
+    return (
         <div className="page-container">
             <div className="page-content">
-                <Column columnTitle="T.I" columnSubTitle="Solicitações de Serviço" textCenter>
-                    
+                <Column
+                    columnTitle="T.I"
+                    columnSubTitle="Solicitações de Serviço"
+                    textCenter
+                >
                     <div className="responsive-container">
-                        <CustomInput name="Nome" type="text" /> 
-                        <CustomInput name="Gestor" type="password"/>
-                        <CustomInput name="E-mail" type="text"/>
+                        <CustomInput name="Nome" type="text" />
+                        <CustomInput name="Gestor" type="password" />
+                        <CustomInput name="E-mail" type="text" />
 
                         <CustomTextArea />
-
                     </div>
-
                 </Column>
             </div>
         </div>
-    )
+    );
 };
 
 export default Technology;
