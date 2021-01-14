4169f537cb0bbcb6d57309b76017c390b26ba711
unknown <lucasvini193@hotmail.com>
2021-01-14 11:25:31 -0300

    Atualização de components e pagina de tech

--- src/pages/Technology/index.js
+++ src/pages/Technology/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
 
+import Column from '../../components/Column';
+// import ContentWrapper from '../../components/ContentWrapper';
+import CustomInput from '../../components/CustomInput';
+
+import './styles.scss';
+
 const Technology = () => {
-    return <div>olá</div>;
+    const teste = 'algumacoisa'
+    return(
+        <div className="page-container">
+            <div className="page-content">
+                <Column columnTitle="T.I" columnSubTitle="Solicitações de Serviço" textCenter>
+                    
+                    <div className="responsive-container">
+                        <CustomInput name="Nome" type="text" /> 
+                        <CustomInput name="Gestor" type="password"/>
+                        <CustomInput name="E-mail" type="text"/>
+                    </div>
+
+                </Column>
+            </div>
+        </div>
+    )
 };
 
 export default Technology;
